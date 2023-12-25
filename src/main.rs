5f57212201ea5b286a26e1b0edae9d2f572cb1b1
marc-chatroux-elmarcofr38 <github.dev@elmarcofr38.net>
2023-12-25 22:12:47 +0100

    Update metamodel file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 /*
 Copyright 2023-2024 CHATROUX MARC
 
-This file is part of Imbriqua Structure.
+This file is part of Imbriqua Structure, a interpreter of BPMN model files (in UML notation) for
+Imbriqua Engine project
 
 Imbriqua Structure is free software: you can redistribute it and/or modify it under the terms of
 the GNU General Public License as published by the Free Software Foundation, either
@@ -16,7 +17,18 @@ If not, see <https://www.gnu.org/licenses/>.
 */
 
 mod module_log;
+mod module_load_classes;
 
 fn main() {
-    println!("Hello, world!");
+
+    // Initialise global logger
+    if module_log::open_module().is_err() {
+        panic!("Error during the loading on logs modules")
+    }
+
+    if module_load_classes::run().is_err() {
+        panic!("Error during the loading of classes")
+    }
+
+    todo!("just, .... TODO !!!")
 }
