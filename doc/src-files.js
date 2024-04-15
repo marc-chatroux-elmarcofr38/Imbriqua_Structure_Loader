ba48bb16e1581106f1b9802ca1be16c7b22f0ac3
marc-chatroux-elmarcofr38 <github.dev@elmarcofr38.net>
2024-04-15 20:09:26 +0200

    Add doc

---
+++ doc/src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = new Map(JSON.parse('[\
+["imbriqua_structure_loader",["",[],["main.rs","module_dependencies_explorer.rs","module_log.rs","module_output_checker.rs"]]]\
+]'));
+createSrcSidebar();
