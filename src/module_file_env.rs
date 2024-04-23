548d825aed9696bd8522a04e503731af9f053061
marc-chatroux-elmarcofr38 <github.dev@elmarcofr38.net>
2024-04-23 14:54:40 +0200

    Reviewing module_file_env

--- src/module_file_env.rs
+++ src/module_file_env.rs
@@ -3,7 +3,7 @@ Copyright 2023-2024 CHATROUX MARC
 
 This file is part of Imbriqua Structure, a interpreter of BPMN model files (in UML notation) for
 Imbriqua Engine project
-
+s
 Imbriqua Structure is free software: you can redistribute it and/or modify it under the terms of
 the GNU General Public License as published by the Free Software Foundation, either
 version 3 of the License, or (at your option) any later version.
@@ -20,8 +20,8 @@ If not, see <https://www.gnu.org/licenses/>.
 #![doc = include_str!("../doc/module_file_env.md")]
 
 // Package section
+use crate::module_file_manager::{FileManager, Path, PathBuf};
 use crate::module_log::*;
-use crate::module_file_manager::{FileManager, PathBuf, Path};
 
 // Dependencies section
 use chrono::Local;
@@ -30,18 +30,17 @@ use chrono::Local;
 #[derive(Clone, PartialEq, Debug)]
 pub struct FileEnv {
     /// Input folder, obtainable with __get_input_folder()__
-    input_folder : PathBuf,
+    input_folder: PathBuf,
     /// Output folder (chilf of main_output_folder, named with time-formatting), obtainable with __get_output_folder()__
-    output_folder : PathBuf,
+    output_folder: PathBuf,
 }
 
 impl FileEnv {
-
-    fn new(input_folder : &str, main_output_folder : &str) -> Self {
+    fn new(input_folder: &str, main_output_folder: &str) -> Self {
         //Set input folder path and output folder path
-        let path_input_folder : PathBuf = Path::new(input_folder).canonicalize_pathbuf();
-        let mut path_output_folder : PathBuf = Path::new(main_output_folder).canonicalize_pathbuf();
-        let time_string : String = Local::now().format("%Y-%m-%d_%Hh%Mm%S/").to_string();
+        let path_input_folder: PathBuf = Path::new(input_folder).canonicalize_pathbuf();
+        let mut path_output_folder: PathBuf = Path::new(main_output_folder).canonicalize_pathbuf();
+        let time_string: String = Local::now().format("%Y-%m-%d_%Hh%Mm%S/").to_string();
         path_output_folder.push(time_string);
         // Create folder
         path_output_folder.create_folder();
@@ -50,15 +49,15 @@ impl FileEnv {
         path_output_folder.check_is_dir();
         // Create instance
         let result = FileEnv {
-            input_folder : path_input_folder,
-            output_folder : path_output_folder,
+            input_folder: path_input_folder,
+            output_folder: path_output_folder,
         };
         // Return result
         result
     }
 
     /// Deleting output folder if empty (for cleaning output main folder)
-    pub fn delete_if_empty (&self) {
+    pub fn delete_if_empty(&self) {
         self.output_folder.delete_folder(true);
     }
 
@@ -74,7 +73,7 @@ impl FileEnv {
 }
 
 /// Shorcut of __FileEnv::new()__, creating FileEnv instance and creating output folder with time name
-pub fn open_env(input_folder : &str, main_output_folder : &str) -> FileEnv {
+pub fn open_env(input_folder: &str, main_output_folder: &str) -> FileEnv {
     FileEnv::new(input_folder, main_output_folder)
 }
 
@@ -96,12 +95,45 @@ pub mod tests {
         let _ = open_env(input_path, main_output_path);
     }
 
-    #[ignore]
-    fn _test_template() {
+    #[test]
+    fn module_env_02_detele_if_empty() {
         // Logs
         initialize_log_for_test();
         // Setting
+        let input_path = "tests/module_file_env/module_env_02_detele_if_empty/input";
+        let main_output_path = "tests/module_file_env/module_env_02_detele_if_empty/main_output";
         // Preparing
+        Path::new(main_output_path).purge_folder();
         // Test
+        let file_env = open_env(input_path, main_output_path);
+        file_env.delete_if_empty();
+    }
+
+    #[test]
+    fn module_env_03_get_input_folder() {
+        // Logs
+        initialize_log_for_test();
+        // Setting
+        let input_path = "tests/module_file_env/module_env_03_get_input_folder/input";
+        let main_output_path = "tests/module_file_env/module_env_03_get_input_folder/main_output";
+        // Preparing
+        Path::new(main_output_path).purge_folder();
+        // Test
+        let file_env = open_env(input_path, main_output_path);
+        file_env.get_input_folder();
+    }
+
+    #[test]
+    fn module_env_04_get_output_folder() {
+        // Logs
+        initialize_log_for_test();
+        // Setting
+        let input_path = "tests/module_file_env/module_env_04_get_output_folder/input";
+        let main_output_path = "tests/module_file_env/module_env_04_get_output_folder/main_output";
+        // Preparing
+        Path::new(main_output_path).purge_folder();
+        // Test
+        let file_env = open_env(input_path, main_output_path);
+        file_env.get_output_folder();
     }
 }
