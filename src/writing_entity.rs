a62d836313af8a91322d5525b120faec43f34ac4
marc-chatroux-elmarcofr38 <github.dev@elmarcofr38.net>
2025-07-08 22:22:48 +0200

    Rebranding tmpl file

--- src/writing_entity.rs
+++ src/writing_entity.rs
@@ -654,7 +654,7 @@ impl CMOFClass {
             let comment = format!("SUPER : ONE {} need ONE {}", class_model_name, model_name,);
             result.push_str(
                 format!(
-                    include_str!("../template/entity_sub_relation_super_from_one.tmpl"),
+                    include_str!("../template/entity_sub_super_relation.tmpl"),
                     table_name = table_name,
                     model_name = model_name,
                     comment = comment,
@@ -776,7 +776,7 @@ impl CMOFClass {
             let comment = format!("SUPER : ONE {} need ONE {}", class_model_name, model_name,);
             result.push_str(
                 format!(
-                    include_str!("../template/entity_sub_related.tmpl"),
+                    include_str!("../template/entity_sub_super_related.tmpl"),
                     table_name = table_name,
                     model_name = model_name,
                     comment = comment,
@@ -802,7 +802,7 @@ impl CMOFClass {
             let comment = format!("SUPER : ONE {} need ONE {}", model_name, class_model_name,);
             result.push_str(
                 format!(
-                    include_str!("../template/entity_sub_related.tmpl"),
+                    include_str!("../template/entity_sub_super_related.tmpl"),
                     table_name = table_name,
                     model_name = model_name,
                     comment = comment,
